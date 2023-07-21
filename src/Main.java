e3a68baa36f0dca67b49a6a39cc73ee2e48078ac
hyunsang-ahn <happylovetkd@naver.com>
2023-07-21 22:20:22 +0900

    java_tutorial_Start

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Date;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3_123_4213_2323L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+
+        Date now = new Date();
+        System.out.println((now));
+
+
+
+    }
+}
